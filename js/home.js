543efca382ca2e19427bc7313e27a75603273271
juliatrue <trju24cc@student.ju.se>
2025-02-19 10:19:25 +0100

    text shadow

--- js/home.js
+++ js/home.js
@@ -5,10 +5,9 @@ contactMe.addEventListener("mouseover", change);
 contactMe.addEventListener("mouseout", changeTwo);
 
 function change() {
-  contactGlow.style.backgroundColor = "#057796";
-  contactGlow.style.borderRadius = "40%";
+  contactGlow.style.textShadow = "2px 0px 8px #FFFFFF";
 }
 
 function changeTwo() {
-  contactGlow.style.backgroundColor = "#004b60";
+  contactGlow.style.textShadow = "none";
 }
